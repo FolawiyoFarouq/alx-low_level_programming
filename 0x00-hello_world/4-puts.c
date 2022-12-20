e4e014ff172653f9bd2fb0212575c1a2c7d87dce
FolawiyoFarouq <folawiyofarouq@gmail.com>
2022-12-20 06:59:47 -0800

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,15 @@
-#!/bin/bash
+/**
+ *
+ *  * main - Entry point
+ *
+ *   *
+ *
+ *    * Return: Always 0 (Success)
+ *
+ *     */
 
-
-
-current_time=$(curl -s "http://worldtimeapi.org/api/ip" | python3 -m json.tool | grep "\"datetime\":" | cut --delimiter=" " --fields=6- | tr -d "\""| tr -d ",")
-puts(\"Programming is like building a multilingual puzzle)
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
