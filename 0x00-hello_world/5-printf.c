8aa505b90013550559a9112ec1b74113761e26de
FolawiyoFarouq <folawiyofarouq@gmail.com>
2022-12-20 06:07:16 -0800

    Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+
+
+/**
+ *
+ *  *  main - main function 
+ *
+ *   *  return always 0
+ *
+ *   */
+
+int main(void)
+
+{
+
+		printf("with proper grammar, but the outcome is a piece of art,");
+
+			return (0);
+
+}
