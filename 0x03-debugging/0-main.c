a68a5f4518a5afabfd20dce2ecd93e7b437ef9d4
FolawiyoFarouq <folawiyofarouq@gmail.com>
2022-12-23 12:05:19 -0800

    Task 0

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,7 +1,6 @@
 #include "main.h"
 
 /**
- *
  * main - tests function that prints if integer is positive or negative
  * Return: 0
  */
@@ -14,4 +13,4 @@ int main(void)
 	positive_or_negative(i);
 
 	return (0);
-}
+i}
