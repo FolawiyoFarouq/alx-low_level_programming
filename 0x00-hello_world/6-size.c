3f1428a99e27c45cb3835c8177d27a5b93501595
FolawiyoFarouq <folawiyofarouq@gmail.com>
2022-12-24 01:34:52 -0800

    Task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,7 +5,7 @@
  * Return: 0 if exited properly, non-zero otherwise
  */
 
-intr main(void)
+int main(void)
 {
 	printf("size of a char: %d byte(s)\n", sizeof(char));
 	printf("size of an int: %d byte(s)\n", sizeof(int));
