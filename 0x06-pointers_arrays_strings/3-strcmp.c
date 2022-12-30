cfa86fc98605be6c32d539626361c9d1ff82ceb7
FolawiyoFarouq <folawiyofarouq@gmail.com>
2022-12-30 09:10:17 -0800

    Task 3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,25 +1,32 @@
 #include "main.h"
 
 /**
+ * _strcmp - compares two strings
  *
- *  * _strcmp - compares two strings
+ * @s1: input parameter
  *
- *   * @s1: input parameter
+ * @s2: input parameter
  *
- *    * @s2: input parameter
  *
- *     *
+ * Return: 0 if equal or b if is different
  *
- *      * Return: 0 if equal or b if is different
- *
- *       */
+ */
 
 int _strcmp(char *s1, char *s2)
 { 
-	while (*s1 && *s2 && *s1 == *s2)
+	while (((*s1 != '\0') && (*s2 != '\0')) && (*s1 == *s2))
 	{
 		s1++;
 		s2++;
 	}
-	return (*s1 + *s2);
+
+	if (*s1 == *s2)
+	{
+		return (0);
+	}
+
+	else
+	{
+		return (*s1 - *s2);
+	}
 }
