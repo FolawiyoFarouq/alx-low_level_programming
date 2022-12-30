8b9bb8ea34711439ed216eef4db4b51e97b534d1
FolawiyoFarouq <folawiyofarouq@gmail.com>
2022-12-30 09:26:19 -0800

    Task 5

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,28 +1,23 @@
 #include "holberton.h"
 
 /**
+ * string_toupper - changes lowercase to uppercase
+ * @str: the string to change
  *
- *  * string_toupper - changes lowercase to uppercase
+ * Return: the string that change for uppercase
  *
- *   * @str: the string to change
- *
- *    *
- *
- *     * Return: the string that change for uppercase
- *
- *      */
+ */
 
-char *string_toupper(char *str)
-
+char *string_toupper(char *s)
 {
-	int index = o;
+	int i;
 
-	while (str[index])
+	i = 0;
+	while (*(s + i))
 	{
-		if (str[index] >= 'a' && str[index] <= 'z')
-			str[index] -= 32;
-
-		index++;
+		if (*(s + i) >= 'a' && *(s + i) <= 'z')
+			*(s + i) -= 'a' - 'A';
+		i++;
 	}
-	return (str);
+	return (s);
 }
