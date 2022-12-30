5a3f6d92391bbae9434a281ea0c17f4008273ee1
FolawiyoFarouq <folawiyofarouq@gmail.com>
2022-12-30 15:16:10 -0800

    Task 6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -3,9 +3,9 @@
 /**
  * cap_string - capitalizes all words of a string
  *
- * @str: the string to change the first letter of a word in uppercase
+ * @s: the string to change the first letter of a word in uppercase
  *
- * Return: capitalizes letters
+ * Return: char
  */
 
 char *cap_string(char *s)
