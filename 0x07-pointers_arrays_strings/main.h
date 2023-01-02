5de9c727b91f60b2eb6c18d3e587ee015530d545
FolawiyoFarouq <folawiyofarouq@gmail.com>
2023-01-02 03:51:23 -0800

    main.h

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,17 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-/**
- * File: main.h
- * Auth: Farouq
- * description: Header file containing prototypes for all functions
- * used in the 0x07. C - Even more pointers, arrays and strings
- */
-
-#include <stddef.h>
-
-int_putchar(char c);
-void *_memset(void *s, int c, size_t n);
+char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
