57512af41c7f1143f96aaa199344aa88575b13da
FolawiyoFarouq <folawiyofarouq@gmail.com>
2023-01-10 12:11:47 -0800

    Task 4

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -1,6 +1,5 @@
 #include <stdlib.h>
 #include "main.h"
-
 /**
  * free_grid - frees the memory allocate for the grid
  * created by alloc_grid()
@@ -12,6 +11,7 @@ void free_grid(int **grid, int height)
 	int i;
 
 	if (grid == NULL || height == 0)
+
 		return;
 
 	for (i = 0; i < height; i++)
