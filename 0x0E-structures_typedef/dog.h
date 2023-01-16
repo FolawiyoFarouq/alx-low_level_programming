12623abb62472c81fb67b598a6c864900809d92d
FolawiyoFarouq <folawiyofarouq@gmail.com>
2023-01-16 10:07:28 -0800

    Task 0

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,25 +1,30 @@
-#ifndef DOG_H
-#define DOG_H
+#ifndef FILE_DOG
+#define FILE_DOG
 
 /**
- * struct dog - a new type: struct dog with the following elements
- * @name: the name of the dog
- * @age: the age of the dog
- * @owner: the owner of the dog
- * Description: a struct that houses a string and a float
+ * struct dog - structure dog
+ * @name: atribute pointer to char name's dog
+ * @age: atribute float age's dog
+ * @owner: atribute pointer to char owner's dog
  */
+
 struct dog
 {
 	char *name;
+
 	float age;
+
 	char *owner;
-}
-void init_dog(struct dog *d, char *name, float age, char *owner);
-void print_dog(struct dog *d);
+};
 
 typedef struct dog dog_t;
 
+void init_dog(struct dog *d, char *name, float age, char *owner);
+
+void print_dog(struct dog *d);
+
 dog_t *new_dog(char *name, float age, char *owner);
+
 void free_dog(dog_t *d);
 
 #endif
