1500c31fde30750375c171a89f63bc663f44d242
FolawiyoFarouq <folawiyofarouq@gmail.com>
2023-01-31 00:16:00 -0800

    Task 8

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -4,7 +4,7 @@
  * sum_listint - a function that returns the sum of all the
  * data (n) of a listint_t linked list
  * @head: the head of the list
- *
+ * Return: returns the sum of all the n's
  */
 int sum_listint(listint_t *head)
 {
